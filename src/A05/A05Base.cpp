8bf2f73243957a73f4271899d1183d87fe65bfe2
Guillermo Sanchez <will_llermo64@hotmail.com>
2017-10-17 10:02:35 +0200

    añadido fondo

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>
 
 //Game general information
 #define SCREEN_WIDTH 800
@@ -8,6 +9,8 @@ int main(int, char*[]) {
 
 	// --- INIT ---
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
+	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
+	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -16,6 +19,9 @@ int main(int, char*[]) {
 	// --- RENDERER ---
 	SDL_Renderer *renderer{ SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
+	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bg.jpg") };
+	if (bgTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
 
 	// --- SPRITES ---
 
@@ -43,6 +49,8 @@ int main(int, char*[]) {
 		// DRAW
 			//Background
 		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderPresent(renderer);
 			//Animated Sprite
 		SDL_RenderPresent(renderer);
 
@@ -51,8 +59,10 @@ int main(int, char*[]) {
 	// --- DESTROY ---
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	SDL_DestroyTexture(bgTexture);
 
 	// --- QUIT ---
+	IMG_Quit();
 	SDL_Quit();
 	return 0;
 }
