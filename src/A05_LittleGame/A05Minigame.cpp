02a51cdf88f52e03711768c477e1926f420ec263
Guillermo Sanchez <will_llermo64@hotmail.com>
2017-10-24 11:12:26 +0200

    movimiento bien

--- src/A05_LittleGame/A05Minigame.cpp
+++ src/A05_LittleGame/A05Minigame.cpp
@@ -162,50 +162,49 @@ void game(SDL_Window *window, SDL_Renderer *renderer) {
 
 	// --- TIME ---
 	clock_t lastTime = clock();
-	float timeDown = 5.;
+	float timeDown = 30.;
 	float deltaTime = 0;
 
 	// --- GAME LOOP ---
 	SDL_Event event;
 	while (gamestat == GameState::PLAY) {
+		const Uint8 *keyboardstate = SDL_GetKeyboardState(NULL);
+		//Player 1
+		if (keyboardstate[SDL_SCANCODE_D] && PlayerPosition.x + PlayerPosition.w < SCREEN_WIDTH) {
+			playerRect.y = frameHeight * 2;
+			PlayerPosition.x += 10;
+		}
+		else if (keyboardstate[SDL_SCANCODE_A] && PlayerPosition.x > 0) {
+			playerRect.y = frameHeight;
+			PlayerPosition.x -= 10;
+		}
+		if (keyboardstate[SDL_SCANCODE_W] && PlayerPosition.y > 130) {
+			playerRect.y = frameHeight * 3;
+			PlayerPosition.y -= 10;
+		}
+		else if (keyboardstate[SDL_SCANCODE_S] && PlayerPosition.y + PlayerPosition.h < SCREEN_HEIGHT) {
+			playerRect.y = frameHeight * 0;
+			PlayerPosition.y += 10;
+		}
+		//Player 2
+		if (keyboardstate[SDL_SCANCODE_RIGHT] && Player2Position.x + Player2Position.w < SCREEN_WIDTH) {
+			player2Rect.y = frameHeight * 6;
+			Player2Position.x += 10;
+		}
+		else if (keyboardstate[SDL_SCANCODE_LEFT] && Player2Position.x > 0) {
+			player2Rect.y = frameHeight * 5;
+			Player2Position.x -= 10;
+		}
+		if (keyboardstate[SDL_SCANCODE_UP] && Player2Position.y > 130) {
+			player2Rect.y = frameHeight * 7;
+			Player2Position.y -= 10;
+		}
+		else if (keyboardstate[SDL_SCANCODE_DOWN] && Player2Position.y + Player2Position.h < SCREEN_HEIGHT) {
+			player2Rect.y = frameHeight * 4;
+			Player2Position.y += 10;
+		}
 			// HANDLE EVENTS
 			while (SDL_PollEvent(&event)) {
-				const Uint8 *keyboardstate = SDL_GetKeyboardState(NULL);
-				const Uint8 *keyboard2state = SDL_GetKeyboardState(NULL);
-				//Player 1
-				if (keyboardstate[SDL_SCANCODE_D] && PlayerPosition.x + PlayerPosition.w < SCREEN_WIDTH) {
-					playerRect.y = frameHeight * 2;
-					PlayerPosition.x += 10;
-				}
-				else if (keyboardstate[SDL_SCANCODE_A] && PlayerPosition.x > 0) {
-					playerRect.y = frameHeight;
-					PlayerPosition.x -= 10;
-				}
-				else if (keyboardstate[SDL_SCANCODE_W] && PlayerPosition.y > 130) {
-					playerRect.y = frameHeight * 3;
-					PlayerPosition.y -= 10;
-				}
-				else if (keyboardstate[SDL_SCANCODE_S] && PlayerPosition.y + PlayerPosition.h < SCREEN_HEIGHT) {
-					playerRect.y = frameHeight * 0;
-					PlayerPosition.y += 10;
-				}
-				//Player 2
-				if (keyboard2state[SDL_SCANCODE_RIGHT] && Player2Position.x + Player2Position.w < SCREEN_WIDTH) {
-					player2Rect.y = frameHeight * 6;
-					Player2Position.x += 10;
-				}
-				else if (keyboard2state[SDL_SCANCODE_LEFT] && Player2Position.x > 0) {
-					player2Rect.y = frameHeight * 5;
-					Player2Position.x -= 10;
-				}
-				else if (keyboard2state[SDL_SCANCODE_UP] && Player2Position.y > 130) {
-					player2Rect.y = frameHeight * 7;
-					Player2Position.y -= 10;
-				}
-				else if (keyboard2state[SDL_SCANCODE_DOWN] && Player2Position.y + Player2Position.h < SCREEN_HEIGHT) {
-					player2Rect.y = frameHeight * 4;
-					Player2Position.y += 10;
-				}
 				switch (event.type) {
 				case SDL_KEYDOWN:
 					break;
