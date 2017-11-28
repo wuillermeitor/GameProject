6c18331f120c48dec740f9ce63df20a0e91b9aba
Guillermo Sanchez <will_llermo64@hotmail.com>
2017-11-28 11:02:28 +0100

    xml ma o meno

---
+++ src/A05_LittleGame/enemy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class enemy
+{
+public:
+	int vitalidad;
+	int daño_de_ataque;
+	int defensa;
+	int frequencia_ataque;
+	int experiencia;
+	enemy();
+	~enemy();
+};
+
